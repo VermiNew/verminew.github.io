289f99c743196bd691fb08a4fcd7ced5c9372327
VermiNew <werminew@gmail.com>
2025-01-28 19:42:44 +0100

    fix: update deployment config to resolve MIME type issues

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,20 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: './',
+  base: '',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
     }
   }
 })
